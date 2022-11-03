61ef5fbc049128dec5c014726e52062ae6a2ea81
HananMohammed <hananmohammed2468@gmail.com>
2022-11-03 22:44:50 +0200

    build db using indexedDB

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,45 @@ export default {
   data() {
     return {
       editor: null,
+      database: null
     }
   },
+  methods:{
+      async getDatatbase()
+      {
+         return new Promise((resolve, reject)=>{
+            //initialize a connection to database and pass it a name like notes and define its version to be 1
+            let db = window.indexedDB.open('notes', 1 )
 
+            db.onerror = (event) => {
+               reject("Error Opening the datatbase !! ")
+            };
+
+            db.onsuccess = (event) => {
+               console.log('db.onsuccess', event);
+               resolve(event.target.result);
+            };
+
+            //Creating or updating the version of the database
+            db.onupgradeneeded = (event) => {
+               console.log('db.onupgradeneeded', event);
+
+               // Save the IDBDatabase interface
+               const db = event.target.result;
+
+               // Create an objectStore for this database
+               const objectStore = db.createObjectStore("notes");
+            };
+         })
+ 
+
+
+      }
+  },
+  async created()
+  {
+      this.database = await this.getDatatbase()
+  },
   mounted() {
     this.editor = new Editor({
       content: '',
