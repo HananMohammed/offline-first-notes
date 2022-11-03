6f663281310cdfd2b5e0259cc8a357ef40026028
HananMohammed <hananmohammed2468@gmail.com>
2022-11-04 00:40:11 +0200

    Saving and displaying notes

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,32 @@
    <div class="flex w-screen h-screen text-gray-700">
       <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
         <!-- sidebar -->
+        <div class="h-0 overflow-auto flex-grow">
+            <div class="mt-4">
+               <a 
+               class="flex items-center h-8 text-sm pl-8 pr-3"
+               href="#"
+               v-for="note in notes"
+               :key="note.created"
+               >
+                  <span class="ml-2 leading-none"> {{ new Date(note.created).toLocaleString()}}</span>
+               </a>
+            </div>
+        </div>
       </div>
       <div class="flex flex-col flex-grow">
          <div class="flex flex-col flex-grow overflow-auto">
             <editor-content :editor="editor" />
          </div>
+         <div class="h-16 text-right">
+            <button @click="saveNote" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Button
+            </button>
+         </div>
+
       </div>
- 
    </div>
+
 </template>
 <script>
    import { Editor, EditorContent } from '@tiptap/vue-3'
@@ -24,7 +42,8 @@ export default {
   data() {
     return {
       editor: null,
-      database: null
+      database: null,
+      notes:[]
     }
   },
   methods:{
@@ -32,7 +51,7 @@ export default {
       {
          return new Promise((resolve, reject)=>{
             //initialize a connection to database and pass it a name like notes and define its version to be 1
-            let db = window.indexedDB.open('notes', 1 )
+            let db = window.indexedDB.open('notes', 3)
 
             db.onerror = (event) => {
                reject("Error Opening the datatbase !! ")
@@ -49,19 +68,53 @@ export default {
 
                // Save the IDBDatabase interface
                const db = event.target.result;
-
+               
+               db.deleteObjectStore("notes")
                // Create an objectStore for this database
-               const objectStore = db.createObjectStore("notes");
+               const objectStore = db.createObjectStore("notes", {
+                  keyPath: 'created'
+               });
             };
          })
- 
+      },
+     async saveNote()
+      {
+         return new Promise((resolve, reject) => {
+            let transaction = this.database.transaction('notes', 'readwrite');
+            transaction.oncomplete = (event) => {
+               resolve()
+            }
 
+            let now = new Date()
+            let note = {
+               content:this.editor.getHTML(),
+               created: now.getTime()    
+            }
 
-      }
+            this.notes.unshift(note)
+
+            transaction.objectStore("notes").add(note);
+
+         })
+      },
+    async getNotes(){
+      return new Promise((resolve, reject) => {
+            this.database
+            .transaction('notes')
+            .objectStore('notes')
+            .getAll()
+            .onsuccess = e => {
+               resolve(e.target.result)
+            }
+
+         })
+    }  
   },
   async created()
   {
       this.database = await this.getDatatbase()
+      let notes = await this.getNotes()
+      this.notes = notes.reverse()
   },
   mounted() {
     this.editor = new Editor({
